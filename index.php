5e93481d0c936678b7beaec964a9928d2b96b151
Risqueira <henrique.mrodrigues77@gmail.com>
2026-06-10 22:13:24 -0300

    iniciando

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    //exemplos de PHP
+    <a href="exemplo1.php"></a>
+    <a href="exemplo2.php"></a>
+    <a href="exemplo3.php"></a>
+
+    //Bloco 1: Algoritmos Sequenciais (Cálculos Simples)<br>
+    <a href="">1 - Conversor de moedas</a><br>
+    <a href="">2 - Calculadora de Área e Perímetro</a><br>
+    <a href="">3 - Consumo de Combustível</a><br>
+
+    <br><br>
+
+    //Bloco 2: Estruturas Condicionais (if/else) <br>
+    <a href="">4 - Situação do Aluno (Média)</a> <br>
+    <a href="">5 - Verificador de Idade (Votação)</a> <br>
+    <a href="">6 - Par ou Ímpar</a> 
+
+    <br><br>
+
+    //Bloco 3: Estruturas Condicionais (switch/case) <br>
+    <a href="">7 - Dia da Semana</a> <br>
+    <a href="">8 - Operações Matemáticas (Calculadora Simples)</a> <br>
+    <a href="">9 - Mês por Extenso</a>
+
+    <br><br>
+
+    //Bloco 4: Laços de Repetição (for ou while) <br>
+    <a href="">10 - Fatorial de um Número</a> <br>
+    <a href="">11 - Somatório de 1 a N</a> <br>
+    <a href="">12 - Sequência de Pares (Intervalo)</a>
+
+    <br><br>
+
+    //Bloco 5: Manipulação de Arrays <br>
+    <a href="">13 - Média de Vários Valores</a> <br>
+    <a href="">14 - Lista de Compras (Checkboxes)</a> <br>
+    <a href="">15 -  Encontrar o Maior Valor</a> <br>
+
+
+
+</body>
+</html>
